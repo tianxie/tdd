276b74c744d88aff5375a04ad53061b5faf9a7f5
Tian Xie <tian.xie3@hp.com>
2015-08-13 17:50:22 +0800

    Chapter 14

--- src/Bank.java
+++ src/Bank.java
@@ -1,7 +1,40 @@
+import java.util.Hashtable;
+
 /**
  */
 public class Bank {
+    private Hashtable rates = new Hashtable();
+
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
     }
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
+    }
+
+    public int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        return (int) rates.get(new Pair(from, to));
+    }
+
+    private class Pair {
+        private String from;
+        private String to;
+
+        Pair(String from, String to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        public boolean equals(Object object) {
+            Pair pair = (Pair) object;
+            return from.equals(pair.from) && to.equals(pair.to);
+        }
+
+        public int hashCode() {
+            return 0;
+        }
+    }
+
 }
