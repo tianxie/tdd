4e85b2fd6ed6427d9e444ec5504e7117db3bb4e8
Tian Xie <tian.xie3@hp.com>
2015-08-13 16:20:32 +0800

    Chapter 9

--- src/Money.java
+++ src/Money.java
@@ -2,9 +2,11 @@
  */
 abstract public class Money {
     protected int amount;
+    protected String currency;
 
-    public Money(int amount) {
+    public Money(int amount, String currency) {
         this.amount = amount;
+        this.currency = currency;
     }
 
     public boolean equals(Object object) {
@@ -14,12 +16,16 @@ abstract public class Money {
     }
 
     public static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     abstract Money times(int multiplier);
 
     public static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
+    }
+
+    public String currency() {
+        return currency;
     }
 }
