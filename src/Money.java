a3b378cb214105d2131318aaeb6c0489a23bef0b
Tian Xie <tian.xie3@hp.com>
2015-08-13 13:26:15 +0800

    Chapter 8

--- src/Money.java
+++ src/Money.java
@@ -1,6 +1,6 @@
 /**
  */
-public class Money {
+abstract public class Money {
     protected int amount;
 
     public Money(int amount) {
@@ -11,5 +11,15 @@ public class Money {
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
+    }
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    abstract Money times(int multiplier);
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
     }
 }
