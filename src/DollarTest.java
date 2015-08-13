9e3f869f88d8789620baaafb6290c183dbe5fb2a
Tian Xie <tian.xie3@hp.com>
2015-08-13 16:37:14 +0800

    Chapter 11

--- src/DollarTest.java
+++ src/DollarTest.java
@@ -34,8 +34,4 @@ public class DollarTest {
         assertEquals("CHF", Money.franc(1).currency());
     }
 
-    @Test
-    public void testDifferentClassEquality() {
-        assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-    }
 }
